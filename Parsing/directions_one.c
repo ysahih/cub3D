e7e83db868e55bd024ebb3cf0a02126f9dfcde7f
Ismail Barka <isbarka@e3r8p9.1337.ma>
2023-08-25 18:19:12 +0100

    norm parsing

--- Parsing/directions_one.c
+++ Parsing/directions_one.c
@@ -1,97 +1,93 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   directions.c                                       :+:      :+:    :+:   */
+/*   directions_one.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: isbarka <isbarka@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/25 18:18:22 by isbarka           #+#    #+#             */
-/*   Updated: 2023/08/25 18:18:40 by isbarka          ###   ########.fr       */
+/*   Created: 2023/08/25 18:18:47 by isbarka           #+#    #+#             */
+/*   Updated: 2023/08/25 18:18:55 by isbarka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cube3d.h"
 
-char	*ft_so(t_data *info)
+void	ft_so_one(t_data *info)
 {
-	char	*to_return;
-	int		i;
+	int	check;
+	int	i;
 
+	check = 0;
 	i = 0;
 	while (info->map2d[i])
 	{
 		if (ft_strncmp(info->map2d[i], "SO ", 3) == 0)
 		{
-			if (info->map2d[i][3])
-				return (ft_strdup(&(info->map2d[i][3])));
+			if (info->map2d[i][3] && check == 0)
+				check = 1;
 			else
 				ft_error();
 		}
 		i++;
 	}
-	ft_error();
-	return (NULL);
 }
 
-char	*ft_we(t_data *info)
+void	ft_we_one(t_data *info)
 {
-	char	*to_return;
-	int		i;
+	int	check;
+	int	i;
 
+	check = 0;
 	i = 0;
 	while (info->map2d[i])
 	{
 		if (ft_strncmp(info->map2d[i], "WE ", 3) == 0)
 		{
-			if (info->map2d[i][3])
-				return (ft_strdup(&(info->map2d[i][3])));
+			if (info->map2d[i][3] && check == 0)
+				check = 1;
 			else
 				ft_error();
 		}
 		i++;
 	}
-	ft_error();
-	return (NULL);
 }
 
-char	*ft_ea(t_data *info)
+void	ft_ea_one(t_data *info)
 {
-	char	*to_return;
-	int		i;
+	int	check;
+	int	i;
 
+	check = 0;
 	i = 0;
 	while (info->map2d[i])
 	{
 		if (ft_strncmp(info->map2d[i], "EA ", 3) == 0)
 		{
-			if (info->map2d[i][3])
-				return (ft_strdup(&(info->map2d[i][3])));
+			if (info->map2d[i][3] && check == 0)
+				check = 1;
 			else
 				ft_error();
 		}
 		i++;
 	}
-	ft_error();
-	return (NULL);
 }
 
-char	*ft_no(t_data *info)
+void	ft_no_one(t_data *info)
 {
-	char	*to_return;
-	int		i;
+	int	check;
+	int	i;
 
+	check = 0;
 	i = 0;
 	while (info->map2d[i])
 	{
 		if (ft_strncmp(info->map2d[i], "NO ", 3) == 0)
 		{
-			if (info->map2d[i][3])
-				return (ft_strdup(&(info->map2d[i][3])));
+			if (info->map2d[i][3] && check == 0)
+				check = 1;
 			else
 				ft_error();
 		}
 		i++;
 	}
-	ft_error();
-	return (NULL);
 }
